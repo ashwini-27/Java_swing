205808a5242f57e7169edea99072b5b4547b380d
ashwini-27 <vashwini409@gmail.com>
2018-06-05 10:31:09 +0530

    facebook login added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import {Facebook} from '@ionic-native/facebook'
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -22,7 +23,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
-    StatusBar,
+    StatusBar,Facebook,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import {Facebook} from '@ionic-native/facebook';
+import firebase from 'firebase';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public facebook:Facebook) {
 
   }
+  Login(): Promise<any> {
+    return this.facebook.login(['email'])
+      .then( response => {
+        const facebookCredential = firebase.auth.FacebookAuthProvider
+          .credential(response.authResponse.accessToken);
+  
+        firebase.auth().signInWithCredential(facebookCredential)
+          .then( success => { 
+            alert("Firebase success: " + JSON.stringify(success)); 
+          });
+  
+      }).catch((error) => { alert((JSON.stringify(error))); });
+  }
+  
+      }
+  
 
-}
